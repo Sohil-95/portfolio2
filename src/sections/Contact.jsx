5e52a616127a4e008d9637c35125a04ed83203fb
Sohil-95 <sohilshaikh9564@gmail.com>
2025-02-26 18:36:51 +0530

    Initial commit

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,100 @@
+import React, { useRef ,useState} from 'react'
+import emailjs from '@emailjs/browser'
+const Contact = () => {
+  const formref =useRef();
+  const [loading, setLoading] = useState(false)
+  const [form, setForm] = useState({
+    name:'',
+    email:'',
+    message:''
+  })
+  const handleChange=({target:{name,value}})=>{
+    setForm({...form,[name]:value})
+  }
+  //service_ael5bui
+  const handleSubmit=async (e)=>{
+    e.preventDefault();
+    try {
+      setLoading(true)
+      await emailjs.send('service_ael5bui','template_o5ri15d',
+      {from_name: form.name,to_name:'Sohil',
+      from_email:form.email,
+      to_email:'sohilshaikh9564@gmail.com',
+      message:form.message},'cxHJZnH6MwKEiaRke')
+
+      setLoading(false)
+      alert('Your message has been sent!')
+
+      setForm({
+        name:'',
+        email:'',
+        message:''
+      })
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+      alert('something went wrong')
+    }
+
+    
+  }
+
+
+  return (
+    <section className='c-space my-20' id='contact'>
+        <div className='relative min-h-screen flex items-center justify-center flex-col'>
+            <img src="/assets/terminal.png" alt="terminal Background"  className='absolute inset min-h-screen'/>
+            <div className='contact-container'>
+                <h3 className='head-text'>Let's talk</h3>
+                <p className='text-lg text-white-600 mt-3'>
+                    Weather you're looking to build a new website ,improve your existing platform,or bring a unique project to life,I'm here to help
+                </p>
+                <form action="" ref={formref} onSubmit={handleSubmit} className='mt-12 flex flex-col space-y-7'>
+                    <label className='space-y-3'>
+                        <span className='field-label'>Full Name</span>
+                        <input
+                          type='text'
+                          name='name'
+                          value={form.name}
+                          onChange={handleChange}
+                          required
+                          className='field-input'
+                          placeholder='John Wick'
+                        />
+                    </label>
+                    <label className='space-y-3'>
+                        <span className='field-label'>Email</span>
+                        <input
+                          type='email'
+                          name='email'
+                          value={form.email}
+                          onChange={handleChange}
+                          required
+                          className='field-input'
+                          placeholder='johnwick12@gmail.com'
+                        />
+                    </label>
+                    <label className='space-y-3'>
+                        <span className='field-label'>Your Message</span>
+                        <textarea
+                          name='message'
+                          value={form.message}
+                          onChange={handleChange}
+                          required
+                          rows={5}
+                          className='field-input'
+                          placeholder="Hi,I wanna give you a job..."
+                        />
+                    </label>
+                    <button className='field-btn' type='submit' disabled={loading}>
+                      {loading ? "Sending..." : "Send Message"}
+                      <img src="/assets/arrow-up.png" alt="arrow-up" className='field-btn_arrow' />
+                    </button>
+                </form>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Contact
